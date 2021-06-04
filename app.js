efa691b0a18c74050061a9cb0da6979cc808c0ce
angelp495 <angeljpa95@gmail.com>
2021-06-04 11:57:05 -0500

    paquetes: request, fetch, axios

--- app.js
+++ app.js
@@ -1,8 +1,38 @@
-const { leerInput } = require("./helpers/inquirer");
+const { leerInput, inquirerMenu, pausa } = require("./helpers/inquirer");
+const Busquedas = require("./models/busquedas");
 
 const main = async () => {
-  const text = await leerInput("Hola");
-  console.log(text);
+  const busquedas = new Busquedas();
+
+  let opt;
+
+  do {
+    opt = await inquirerMenu();
+
+    switch (opt) {
+      case 1:
+        // Mostrar mensaje
+        const lugar = await leerInput("Ciudad: ");
+        console.log(lugar);
+        // Buscar mostrar lugares
+
+        // Seleccionar el lugar
+
+        // Obtener info
+
+        // Mostrar resultados
+        console.log(`\n Información de la ciudad\n`.green);
+        console.log("Ciudad: ", "datos");
+        console.log("Latitud: ", "datos");
+        console.log("Longitud: ", "datos");
+        console.log("Temperatura: ");
+        console.log("Minima: ", "datos");
+        console.log("Máxima: ", "datos");
+        break;
+    }
+
+    if (opt !== 0) await pausa();
+  } while (opt !== 0);
 };
 
 main();
---
+++ models/busquedas.js
@@ -0,0 +1,16 @@
+class Busquedas {
+  historial = ["Madrid", "Monterrey", "Toronto"];
+
+  constructor() {
+    // TODO: leer DB si existe
+  }
+
+  async ciudad(lugar = "") {
+    // petición http
+    console.log(lugar);
+
+    return [];
+  }
+}
+
+module.exports = Busquedas;
